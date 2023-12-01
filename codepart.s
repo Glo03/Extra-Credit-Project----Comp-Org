1fa31268da08b99ee742253906732e9c49936a9a
Glo03 <110479000+Glo03@users.noreply.github.com>
2023-11-30 22:34:24 -0500

    completed the prompts

--- codepart.s
+++ codepart.s
@@ -1,1 +1,5 @@
-
+.data
+    promptL:    .asciiz "Enter the value of L (> 20): "
+    promptM:    .asciiz "Enter the value of M (> 20): "
+    promptN:    .asciiz "Enter the value of N (> 20): "
+    error_msg:      .asciiz "Illegal Number!\n"
