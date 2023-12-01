e6b0ed8bd7a239c55d2f0797f1a42f504a86f1d7
Glo03 <110479000+Glo03@users.noreply.github.com>
2023-11-30 22:35:46 -0500

    adding the GCD code

--- codepart.s
+++ codepart.s
@@ -3,3 +3,14 @@
     promptM:    .asciiz "Enter the value of M (> 20): "
     promptN:    .asciiz "Enter the value of N (> 20): "
     error_msg:      .asciiz "Illegal Number!\n"
+
+.text
+.globl main
+    # Function to calculate GCD
+    gcd:
+        # Inputs: $a0 = first number, $a1 = second number
+        # Outputs: $v0 = GCD
+        # Temp registers: $t0, $t1
+
+        move $t0, $a0         # Copy first number to $t0
+        move $t1, $a1         # Copy second number to $t1
